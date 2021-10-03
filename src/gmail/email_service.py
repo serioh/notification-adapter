c64e7e7a41e31c4bbc0b237fd5089265e100e617
leolebleis <leo.lebleis@gmail.com>
2021-10-03 17:40:03 +0100

    General reformatting and tests WIP

--- src/gmail/email_service.py
+++ src/gmail/email_service.py
@@ -2,25 +2,73 @@ import base64
 import os.path
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from typing import Any
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient import errors
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
+
 
 path_to_here = os.path.dirname(os.path.abspath(__file__))
-credentials_path = os.path.join(path_to_here, 'credentials.json')
-token_path = os.path.join(path_to_here, 'token.json')
+credentials_path = os.path.join(path_to_here, "credentials.json")
+token_path = os.path.join(path_to_here, "token.json")
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 
-def authenticate() -> Any:
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
+class EmailService:
+    def __init__(self, gmail_client: Resource) -> None:
+        super().__init__()
+        self._gmail_client = gmail_client
+
+    @staticmethod
+    def _create_message(
+        recipients: list[str], subject: str, html_message: str
+    ) -> dict[str, str]:
+        """Create a message for an email.
+
+        Args:
+            recipients: Email addresses of the recipients.
+            subject: The subject of the email message.
+            html_message: The text of the email message.
+
+        Returns:
+            An object containing a base64url encoded email object.
+        """
+        message = MIMEMultipart("alternative")
+        message["to"] = ", ".join(recipients)
+        message["from"] = "me"
+        message["subject"] = subject
+        part1 = MIMEText(html_message, "html")
+        message.attach(part1)
+        return {"raw": base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+    def send_email(self, recipients: list[str], subject: str, html_message: str) -> str:
+        """Send email using the gmail API.
+
+        Args:
+            recipients: Email addresses of the recipients.
+            subject: The subject of the email message.
+            html_message: The text of the email message.
+
+        Returns:
+            The response from the Google API.
+        """
+        message = self._create_message(recipients, subject, html_message)
+
+        google_response = (
+            self._gmail_client.users()
+            .messages()
+            .send(userId="me", body=message)
+            .execute()
+        )
+        response: str = f"Message Id: {google_response['id']}"
+        return response
+
+
+def authenticate_with_gmail() -> Resource:
+    """Authenticates with the Gmail API using local credentials files.
     """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
@@ -34,53 +82,9 @@ def authenticate() -> Any:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                credentials_path, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open(token_path, 'w') as token:
+        with open(token_path, "w") as token:
             token.write(creds.to_json())
-
-    return build('gmail', 'v1', credentials=creds)
-
-
-def create_message(sender: str, recipients: list[str], subject: str, message_text: str) -> dict[str, str]:
-    """Create a message for an email.
-
-  Args:
-    sender: Email address of the sender.
-    recipients: Email addresses of the recipients.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
-
-  Returns:
-    An object containing a base64url encoded email object.
-  """
-    message = MIMEMultipart('alternative')
-    message['to'] = ", ".join(recipients)
-    message['from'] = sender
-    message['subject'] = subject
-    part1 = MIMEText(message_text, 'html')
-    message.attach(part1)
-    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
-
-
-def send_message(service: Any, user_id: str, message: dict[str, str]) -> str:
-    """Send an email message.
-
-    Args:
-      service: Authorized Gmail API service instance.
-      user_id: User's email address. The special value "me"
-      can be used to indicate the authenticated user.
-      message: Message to be sent.
-
-    Returns:
-      Sent Message.
-    """
-    try:
-        google_response = service.users().messages().send(userId=user_id, body=message).execute()
-        response: str = f"Message Id: {google_response['id']}"
-        print(response)
-        return response
-    except errors.HttpError as error:
-        print(f"An error occurred: {error}")
+    return build("gmail", "v1", credentials=creds)
--- tests/test_gmail.py
+++
@@ -1,9 +0,0 @@
-from src.gmail.email_service import create_message
-
-
-def test_create_message() -> None:
-    try:
-        message = create_message('sender', ['to'], 'subject', 'message_text')
-        assert message is not None
-    except Exception as exc:
-        assert False
